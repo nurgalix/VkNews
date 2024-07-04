45b1a99937edf1162d191b32892060a4551cbe01
Nurgali Nuraly <nurali.nurgali@gmail.com>
2024-07-04 12:08:48 +0500

    Initial Commit

---
+++ VkNews/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VkNews
+//
+//  Created by Nurgali on 04.07.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
